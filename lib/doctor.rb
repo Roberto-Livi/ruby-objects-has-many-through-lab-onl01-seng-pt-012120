4d108ee8061fe56b45fb4cc2d2507de8fd1d9399
Roberto Livi <robertomlivi@outlook.com>
2020-02-13 22:03:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,4 +17,8 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
+  def new_appointment(patient, date)
+    appointment = Appointment.new(patient, self, date)
+  end
+  
 end
